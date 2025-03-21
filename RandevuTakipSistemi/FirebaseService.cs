08752a9d54931deeede073f79531603355b584f4
Ahmet Güvendik <ahmetguvendik01@gmail.com>
2025-03-22 00:01:41 +0300

    StartingProject
    Firebase Bağlantısı Tamamlandı

---
+++ RandevuTakipSistemi/FirebaseService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Firestore;
+
+namespace RandevuTakipSistemi
+{
+
+    public class FirebaseService
+    {
+        public FirestoreDb FirestoreDb { get; private set; }
+
+        public FirebaseService(string jsonPath)
+        {
+            // FirebaseApp zaten başlatılmadıysa başlat
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "randevutakipsistemi-24565-firebase-adminsdk-fbsvc-e0c50fa208.json");
+            if (FirebaseApp.DefaultInstance == null)
+            {
+                FirebaseApp.Create(new AppOptions()
+                {
+                    Credential = GoogleCredential.FromFile(jsonPath)
+                });
+            }
+
+            // Firestore bağlantısını kur
+            FirestoreDb = FirestoreDb.Create("randevutakipsistemi-24565");
+        }
+    }
+
+}
+
---
+++ RandevuTakipSistemi/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Google.Cloud.Firestore;
+
+namespace RandevuTakipSistemi
+{
+    public partial class Form1 : Form
+    {
+        private FirestoreDb db;
+        public Form1()
+        {
+            InitializeComponent();
+            // Firebase bağlantısını kur
+            FirebaseService firebaseService = new FirebaseService("randevutakipsistemi-24565-firebase-adminsdk-fbsvc-e0c50fa208.json");
+            db = firebaseService.FirestoreDb;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            // TextBox'lardan verileri al
+            string name = textBox1.Text;
+            string phone = textBox2.Text;
+
+            // Firestore'a eklenecek veri
+            var user = new
+            {
+                Name = name,
+                Phone = phone
+            };
+
+            // Firestore'a veri ekle
+            DocumentReference addedUserRef = await db.Collection("users").AddAsync(user);
+            MessageBox.Show("Kullanıcı başarıyla eklendi!");
+        }
+    }
+}
